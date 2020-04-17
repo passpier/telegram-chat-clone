6e9d281121ad0321466dd5eec5012de62d823024
passpier <poo9810@gmail.com>
2020-04-17 10:03:49 +0800

    add iOS project

---
+++ tgChatClone/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tgChatClone
+//
+//  Created by vincent cheng on 2020/4/17.
+//  Copyright © 2020 PSC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
